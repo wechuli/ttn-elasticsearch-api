915480fe58ac94a73df54b8b4e0320f56462dde8
wechuli <wechulipaul@yahoo.com>
2019-02-13 15:55:48 +0300

    changes to route

--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -3,21 +3,26 @@ const client = require("../elastic/connection");
 
 const router = express.Router();
 
-router.post("/", async (req, res) => {
-  const data = req.body;
-  // console.log(data);
-  
-  try {
-    const response = await client.index({
-      index: "iotdata",
-      type: "all",
-      body: data
-    });
-    // console.log(response);
-    return res.status(200).json({ message: "all good" });
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+router
+  .route("/")
+  .post(async (req, res) => {
+    const data = req.body;
+    // console.log(data);
+
+    try {
+      const response = await client.index({
+        index: "iotdata",
+        type: "all",
+        body: data
+      });
+      // console.log(response);
+      return res.status(200).json({ message: "all good" });
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  })
+  .get(async (req, res) => {
+    return res.status(200).json({ message: "all good, you can now get" });
+  });
 
 module.exports = router;
